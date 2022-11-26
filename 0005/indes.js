3ff16a1b1340aa3ada94e0fb0b578e23791cff17
megafriday <megafriday@yahoo.co.jp>
2022-11-27 00:25:55 +0900

    2022-11-27の作業

--- 0005/indes.js
+++ 0005/indes.js
@@ -23,7 +23,15 @@ function reverseWords2(str) {
 	return str.split(" ").map(s => s.split("").reverse().join("")).join(" ");
 }
 
+/**
+ * @param {string} str 
+ */
+function reverseWords3(str) {
+	return str.replace(/\S+/g, s => [...s].reverse().join(""));
+}
+
 module.exports = {
 	reverseWords1,
-	reverseWords2
+	reverseWords2,
+	reverseWords3
 }
--- 0005/index.test.js
+++ 0005/index.test.js
@@ -1,4 +1,4 @@
-const { reverseWords1, reverseWords2 } = require('./indes');
+const { reverseWords1, reverseWords2, reverseWords3 } = require('./indes');
 
 describe("0005のテスト", () => {
 	test("reverseWords1のテスト", () => {
@@ -23,5 +23,17 @@ describe("0005のテスト", () => {
 		expect(reverseWords2('a b c d')).toBe('a b c d');
 
 		expect(reverseWords2('double  spaced  words')).toBe('elbuod  decaps  sdrow')
+	});
+
+	test("reverseWords3のテスト", () => {
+
+		expect(reverseWords3('The quick brown fox jumps over the lazy dog.'))
+			.toBe('ehT kciuq nworb xof spmuj revo eht yzal .god');
+
+		expect(reverseWords3('apple')).toBe('elppa');
+
+		expect(reverseWords3('a b c d')).toBe('a b c d');
+
+		expect(reverseWords3('double  spaced  words')).toBe('elbuod  decaps  sdrow')
 	});
 });
