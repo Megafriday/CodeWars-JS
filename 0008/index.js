0e424c800272b6e7bf454f7273a8e17540b16b36
megafriday <megafriday@yahoo.co.jp>
2023-01-23 02:25:29 +0900

    2023-01-23の作業

---
+++ 0008/index.js
@@ -0,0 +1,56 @@
+/**
+ * @param {string} str 
+ */
+function domainName(str) {
+	if (!str.includes("http")) str = "http://" + str;
+
+	const url = new URL(str);
+	return url.hostname
+		.split(".")
+		.filter(s => !s.includes("www"))[0];
+}
+
+/**
+ * @param {string} str
+ */
+function domainName1(str) {
+	str = str.replace("https://", "");
+	str = str.replace("http://", "");
+	str = str.replace("www.", "");
+	return str.split(".")[0];
+}
+
+/**
+ * @param {string} str
+ */
+function domainName2(str) {
+	return str.replace("https://", "")
+		.replace("http://", "")
+		.replace("www.", "")
+		.split(".")[0];
+}
+
+/**
+ * @param {string} str
+ */
+function domainName3(str) {
+	return str.replace(/(https?:\/\/)?(www\.)?/g, "")
+		.split(".")[0];
+}
+
+/**
+ * @param {string} str
+ */
+function domainName4(str) {
+	return str.replace(/.+\/\/|www\.|\..+/g, "");
+}
+
+console.log(domainName("www.xakep.ru"));
+
+module.exports = {
+	domainName,
+	domainName1,
+	domainName2,
+	domainName3,
+	domainName4
+}
---
+++ 0008/index.test.js
@@ -0,0 +1,44 @@
+const { domainName, domainName1, domainName2, domainName3, domainName4 } = require('./index');
+
+describe("0008のテスト", () => {
+	test("domainNameのテスト", () => {
+		expect(domainName("http://google.com")).toBe("google");
+		expect(domainName("http://google.co.jp")).toBe("google");
+		expect(domainName("www.xakep.ru")).toBe("xakep");
+		expect(domainName("https://youtube.com")).toBe("youtube");
+		expect(domainName("https://www.e0-4b07e.pro/archive/")).toBe("e0-4b07e");
+	});
+
+	test("domainName1のテスト", () => {
+		expect(domainName1("http://google.com")).toBe("google");
+		expect(domainName1("http://google.co.jp")).toBe("google");
+		expect(domainName1("www.xakep.ru")).toBe("xakep");
+		expect(domainName1("https://youtube.com")).toBe("youtube");
+		expect(domainName1("https://www.e0-4b07e.pro/archive/")).toBe("e0-4b07e");
+	});
+
+	test("domainName2のテスト", () => {
+		expect(domainName2("http://google.com")).toBe("google");
+		expect(domainName2("http://google.co.jp")).toBe("google");
+		expect(domainName2("www.xakep.ru")).toBe("xakep");
+		expect(domainName2("https://youtube.com")).toBe("youtube");
+		expect(domainName2("https://www.e0-4b07e.pro/archive/")).toBe("e0-4b07e");
+	});
+
+	test("domainName3のテスト", () => {
+		expect(domainName3("http://google.com")).toBe("google");
+		expect(domainName3("http://google.co.jp")).toBe("google");
+		expect(domainName3("www.xakep.ru")).toBe("xakep");
+		expect(domainName3("https://youtube.com")).toBe("youtube");
+		expect(domainName3("https://www.e0-4b07e.pro/archive/")).toBe("e0-4b07e");
+	});
+
+	test("domainName4のテスト", () => {
+		expect(domainName4("http://google.com")).toBe("google");
+		expect(domainName4("http://google.co.jp")).toBe("google");
+		expect(domainName4("www.xakep.ru")).toBe("xakep");
+		expect(domainName4("https://youtube.com")).toBe("youtube");
+		expect(domainName4("https://www.e0-4b07e.pro/archive/")).toBe("e0-4b07e");
+	});
+
+});
